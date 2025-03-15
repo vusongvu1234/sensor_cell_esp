cbf025ca6c00b0df306bdcb1d0f1ac1544e368d5
vusongvu1234 <vuthiencuc@gmail.com>
2025-03-15 10:54:17 +0700

    Create install_dependencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+arduino-cli lib install "Adafruit GFX Library"
+arduino-cli lib install "Adafruit ST7735 and ST7789 Library"
+arduino-cli lib install "Adafruit SHT31 Library"
+arduino-cli lib install "Adafruit MPU6050 Library"
+arduino-cli lib install "VL53L0X"
+arduino-cli lib install "LittleFS"
+echo "✅ Tất cả thư viện đã được cài đặt!"
